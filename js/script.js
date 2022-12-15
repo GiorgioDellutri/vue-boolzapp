c291205a696a9aba55398941cbe9533253837ac3
GiorgioDellutri <giorgiodll93@gmail.com>
2022-12-15 11:48:02 +0100

    index

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const {createApp} = Vue; 
+
+createApp({
+    data() {
+        return{
+            todo : [
+                    {
+                        text:'Create website',
+                        done: true
+                    }, 
+                    {
+                        text:'Sponsor page',
+                        done: false
+                    },
+                    {
+                        text:'Create logos',
+                        done: true
+                    },
+                    {
+                        text:'Buy domain',
+                        done: true 
+                    },
+                    {
+                        text:'Take money $',
+                        done: false 
+                    },
+                ],
+                strikeThrough:'strike-through ',
+        }
+    },
+
+    methods: {
+        removeItem(itemToRemove){
+            itemIndex = (this.todo.indexOf(itemToRemove))
+            if (itemIndex > -1){
+                this.todo.splice(itemIndex, 1)
+            }
+        },
+        addNewItem(inputUser){
+            this.clearUserInput();
+            this.todo.push({
+                text:inputUser,
+                done: false 
+            })
+        },
+        clearUserInput(){
+            this.newTodoItem= '';
+        },
+        clickThrough(){
+            element.done = !element.done
+        }
+    }
+}).mount('#app')
+
